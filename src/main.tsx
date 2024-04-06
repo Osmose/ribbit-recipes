9ca8e9caa0940077f5d4ac75223ffc9d01ba9eb7
Osmose <me@mkelly.me>
2024-04-06 13:23:07 -0700

    Pulling test data from supabase

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createClient } from '@supabase/supabase-js';
+
 import App from './App.tsx';
 import './index.css';
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_PROJECT_URL, import.meta.env.VITE_SUPABASE_API_KEY);
+const { data, error } = await supabase.from('testdata').select();
+console.log(data);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
